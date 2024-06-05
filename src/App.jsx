af8a3b0d5c069afe7b53328af40fc6d26ff7c7eb
Tiago Santana Palmeira <125421932+Tiagopalmeira@users.noreply.github.com>
2024-06-05 10:10:35 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function QRScanner() {
   const startQrScanner = () => {
     const html5QrCodeScanner = new Html5QrcodeScanner(
       "reader",
-      { fps: 10, qrbox: { width: 250, height: 250 } },
+      { fps: 10, qrbox: { width: 20, height: 250 } },
       false // verbose
     );
 
